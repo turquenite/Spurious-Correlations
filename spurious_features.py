07f38142bb956d81558d459456b64f0969da2e01
Turquenite <dominik.karasin@outlook.de>
2024-10-29 10:31:45 +0100

    ✨ Add spurious lines function

--- spurious_features.py
+++ spurious_features.py
@@ -10,24 +10,50 @@ class Position(Enum):
     RIGHT_BOTTOM = "right_bottom"
 
 
+class Orientation(Enum):
+    HORIZONTAL = "horizontal"
+    VERTICAL = "vertical"
+
+
 def spurious_square(
     image: np.ndarray, pos: Position = Position.LEFT_TOP, size: int = 3
 ):
     _, height, width = image.shape
     match pos:
         case Position.LEFT_TOP:
-            image[:size, :size] = 1
+            image[0, :size, :size] = 1
 
         case Position.LEFT_BOTTOM:
-            image[height - size :, :size] = 1
+            image[0, height - size :, :size] = 1
 
         case Position.RIGHT_TOP:
-            image[:size, width - size :] = 1
+            image[0, :size, width - size :] = 1
 
         case Position.RIGHT_BOTTOM:
-            image[height - size :, width - size :] = 1
+            image[0, height - size :, width - size :] = 1
 
         case _:
             raise ValueError("Invalid Position.")
+
+    return image
+
+
+def spurious_lines(
+    image: np.ndarray,
+    orientation: Orientation = Orientation.VERTICAL,
+    distance: int = 7,
+):
+    _, height, width = image.shape
+    match orientation:
+        case Orientation.VERTICAL:
+            for x in range(0, width, distance):
+                image[0, :, x] = 1
+
+        case Orientation.HORIZONTAL:
+            for y in range(0, height, distance):
+                image[0, y, :] = 1
+
+        case _:
+            raise ValueError("Invalid Orientation")
 
     return image
