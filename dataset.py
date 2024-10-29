6539623b7b144a12c4f7a83d24c2c9a7adb8bbae
Turquenite <dominik.karasin@outlook.de>
2024-10-29 10:32:15 +0100

    🚀 Improve and fix Dataset

--- dataset.py
+++ dataset.py
@@ -1,9 +1,9 @@
+import random
+
 import matplotlib.pyplot as plt
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
-
-from spurious_features import Position, spurious_square
 
 
 class MNISTDataset(Dataset):
@@ -12,16 +12,19 @@ class MNISTDataset(Dataset):
         train: bool = True,
         labels: None | list[int] = None,
         spurious_features: None | dict[int : list[callable, float]] = None,
-        *args,
-        **kwargs,
+        random_seed: int = None,
     ):
         self.train = train
         self.spurious_features = spurious_features
+        self.spurious_indices = {}
         self.transform = transforms.Compose(
             [
                 transforms.ToTensor(),
             ]
         )
+
+        if random_seed:
+            random.seed(random_seed)
 
         full_dataset = datasets.MNIST(
             root="mnist_data", train=self.train, download=True, transform=self.transform
@@ -31,21 +34,34 @@ class MNISTDataset(Dataset):
             indices = [
                 i for i, (_, label) in enumerate(full_dataset) if label in labels
             ]
-            self.data = torch.utils.data.Subset(full_dataset, indices)
+            data_subset = torch.utils.data.Subset(full_dataset, indices)
+            data = [(img, label) for img, label in data_subset]
 
         else:
-            self.data = full_dataset
+            data = [(img, label) for img, label in full_dataset]
+
+        if spurious_features:
+            for spurious_label in spurious_features:
+                spurious_function, probability = spurious_features[spurious_label]
+                indices = [
+                    i for i, (_, label) in enumerate(data) if label == spurious_label
+                ]
+
+                num_choices = int(len(indices) * probability)
+                selected_indices = random.choices(indices, k=num_choices)
+
+                self.spurious_indices[spurious_label] = selected_indices
+
+                for i in selected_indices:
+                    data[i] = (spurious_function(data[i][0]), data[i][1])
+
+        self.data = data
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        image, label = self.data[idx]
-
-        if label in self.spurious_features:
-            image = self.spurious_features[label](image)
-
-        return image, label
+        return self.data[idx]
 
     def view_item(self, idx):
         image, label = self.data[idx]
@@ -57,27 +73,3 @@ class MNISTDataset(Dataset):
         plt.title(f"Label: {label}")
         plt.axis("off")
         plt.show()
-
-
-if __name__ == "__main__":
-    # Create instances for training and testing datasets
-    train_dataset = MNISTDataset(
-        train=True,
-        labels=[9, 7],
-        spurious_features={
-            9: lambda img: spurious_square(img, pos=Position.RIGHT_TOP, size=3)
-        },
-    )
-    test_dataset = MNISTDataset(train=False, labels=[0, 7])
-
-    # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-    # Test iterating over a batch
-    for images, labels in train_loader:
-        print(f"Batch of images has shape: {images.shape}")
-        print(f"Batch of labels has shape: {labels.shape}")
-        break
-
-    train_dataset.view_item(3)
