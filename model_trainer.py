970cac22723f691e401b67ae2dfdddf771af65ab
turquenite <dominik.karasin@outlook.de>
2024-11-03 18:44:20 +0100

    🚀 Add accuracy metric

--- model_trainer.py
+++ model_trainer.py
@@ -35,7 +35,7 @@ def train(
 
     optimizer = optimizer_type(model.parameters(), lr)
 
-    device = "cuda" if False else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model = model.to(device)
 
     best_vloss = 1_000_000.0
@@ -51,18 +51,20 @@ def train(
     for _ in range(num_epochs):
         print("EPOCH {}:".format(epoch_number + 1))
 
-        # Make sure gradient tracking is on, and do a pass over the data
+        # Train Mode
         model.train(True)
-        avg_loss = _train_one_epoch(
+        avg_loss, avg_accuracy = _train_one_epoch(
             epoch_number, writer, model, train_loader, loss_function, optimizer, device
         )
 
         running_vloss = 0.0
-        # Set the model to evaluation mode, disabling dropout and using population
-        # statistics for batch normalization.
+        num_predictions = 0
+        num_correct_predictions = 0
+
+        # Evaluation Mode
         model.eval()
 
-        # Disable gradient computation and reduce memory consumption.
+        # Disable gradient computation
         with torch.no_grad():
             for i, vdata in enumerate(validation_loader):
                 vinputs, vtrue_labels, vencoded_labels, vspurious = vdata
@@ -76,16 +78,34 @@ def train(
                 vloss = loss_function(voutputs, vencoded_labels)
                 running_vloss += vloss.item()
 
-        avg_vloss = float(running_vloss / (i + 1))
-        print("LOSS train {} valid {}".format(avg_loss, avg_vloss))
+                _, predicted_labels = torch.max(voutputs, 1)
+                num_correct_predictions += (
+                    (predicted_labels == vencoded_labels).sum().item()
+                )
+                num_predictions += vencoded_labels.size(0)
 
-        # Log the running loss averaged per batch
-        # for both training and validation
+        avg_vloss = float(running_vloss / (i + 1))
+        avg_vaccuracy = num_correct_predictions / num_predictions
+
+        print(
+            "LOSS train {} valid {} | ACCURACY train {} valid {}".format(
+                avg_loss, avg_vloss, avg_accuracy, avg_vaccuracy
+            )
+        )
+
+        # Log the running loss and accuracy averaged per batch
         writer.add_scalars(
             "Training vs. Validation Loss",
             {"Training": avg_loss, "Validation": avg_vloss},
             epoch_number + 1,
         )
+
+        writer.add_scalars(
+            "Accuracy",
+            {"Training": avg_accuracy, "Validation": avg_vaccuracy},
+            epoch_number + 1,
+        )
+
         writer.flush()
 
         # Track best performance, and save the model's state
@@ -109,6 +129,8 @@ def _train_one_epoch(
 ):
     running_loss = 0.0
     last_loss = 0.0
+    num_predictions = 0
+    num_correct_predictions = 0
 
     N = len(train_loader) // 2 - 1
 
@@ -129,6 +151,12 @@ def _train_one_epoch(
         # Make predictions for this batch
         outputs = model(inputs)
 
+        # Fetch predicted labels for computing accuracy labels later
+        _, predicted_labels = torch.max(outputs, 1)
+
+        num_correct_predictions += (predicted_labels == encoded_labels).sum().item()
+        num_predictions += encoded_labels.size(0)
+
         # Compute the loss and its gradients
         loss = loss_function(outputs, encoded_labels)
         loss.backward()
@@ -141,9 +169,16 @@ def _train_one_epoch(
 
         if i % N == N - 1:
             last_loss = running_loss / N
-            print("  batch {} loss: {}".format(i + 1, last_loss))
+            accuracy = num_correct_predictions / num_predictions
+
+            print("  batch {} loss: {} accuracy: {}".format(i + 1, last_loss, accuracy))
+
             tb_x = current_epoch * len(train_loader) + i + 1
             tb_writer.add_scalar("Loss/train", last_loss, tb_x)
+            tb_writer.add_scalar("Accuracy/train", accuracy, tb_x)
+
             running_loss = 0.0
 
-    return last_loss
+    avg_loss = running_loss / len(train_loader)
+    avg_accuracy = num_correct_predictions / num_predictions
+    return avg_loss, avg_accuracy
