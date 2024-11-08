be57f801e186b6b6d00e2ef746bd6fc7327ce421
Turquenite <dominik.karasin@outlook.de>
2024-11-08 12:33:44 +0100

    🚀Update sandbox

--- sandbox.py
+++ sandbox.py
@@ -10,21 +10,20 @@ from spurious_features import Position, spurious_square
 if __name__ == "__main__":
     train_dataset = MNISTDataset(
         train=True,
-        labels=[9, 7],
+        labels=[9, 7, 6],
         spurious_features={
-            9: [
-                lambda img: spurious_square(img, pos=Position.LEFT_TOP, size=4),
-                0.8,
-            ]
+            9: lambda img: spurious_square(img, pos=Position.LEFT_TOP, size=6),
+            7: lambda img: spurious_square(img, pos=Position.LEFT_TOP, size=6),
         },
+        probabilities={9: 0.99, 7: 0.01},
     )
-    validation_dataset = MNISTDataset(train=False, labels=[9, 7])
+    validation_dataset = MNISTDataset(train=False, labels=[9, 7, 6])
 
     # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
     validation_loader = DataLoader(validation_dataset, batch_size=64, shuffle=False)
 
-    model = SimpleModel(num_classes=2)
+    model = SimpleModel(num_classes=3)
 
     train(
         model=model,
